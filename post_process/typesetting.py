8300c263ac89a49cef9aa01e9770bd157ee66c38
daveying <daveying@163.com>
2018-10-17 23:37:01 +0800

    get height median

--- post_process/typesetting.py
+++ post_process/typesetting.py
@@ -1,5 +1,14 @@
+# coding:utf-8
 import json
 
+def choose_x(elem):
+    return elem[0]
+def choose_y(elem):
+    return elem[1]
+def choose_w(elem):
+    return elem[3]
+def choose_h(elem):
+    return elem[4]
 
 def typesetting(text_areas):
     '''
@@ -7,10 +16,21 @@ def typesetting(text_areas):
                         text area width height and text string
                         array of [x, y, w, h, text_string]
     '''
+    text_area_num = len(text_areas)
+    text_areas.sort(key = choose_h)
+    height_median = 0
+    if text_area_num % 2 == 1:
+        idx = int((text_area_num + 1) / 2) - 1
+        height_median = text_areas[idx][3]
+    else:
+        idx = int(text_area_num / 2) - 1
+        height_median = (text_areas[idx][3] + text_areas[idx + 1][3]) * 0.5
+    text_areas.sort(key = choose_y)
+    print json.dumps(text_areas)
 
 if __name__ == '__main__':
     json_str = ''
-    with open('result.json') as f:
+    with open('example.json') as f:
         lines = f.readlines()
         for line in lines:
             json_str += line
